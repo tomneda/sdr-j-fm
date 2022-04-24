3d2c5f37d8fb8fb309fc2e47f79be65a315652fa
Thomas Neder <thomas.neder@freenet.de>
2022-04-24 20:06:57 +0200

    Beautify fir-fir-filters
    (cherry picked from commit 8b5775e9610ff2b75bff0206b5cd2b24eaeb09cf)

--- includes/various/fir-filters.h
+++ includes/various/fir-filters.h
@@ -1,4 +1,3 @@
-#
 /*
  *
  *    Copyright (C) 2010, 2011, 2012
@@ -29,158 +28,188 @@
 #ifndef FIR_FILTERS
 #define FIR_FILTERS
 
-#include	"fm-constants.h"
-#include	<stdlib.h>
-#include	<math.h>
-#include	"fft.h"
+#include  "fm-constants.h"
+#include  <stdlib.h>
+#include  <math.h>
+#include  "fft.h"
 
-class	HilbertFilter;
+class HilbertFilter;
 
-class	Basic_FIR	{
-	public:
-	int16_t		filterSize;
-	DSPCOMPLEX	*filterKernel;
-	DSPCOMPLEX	*Buffer;
-	int16_t		ip;
-	int32_t		sampleRate;
-
-			Basic_FIR (int16_t size) {
-	int16_t		i;
-	filterSize	= size;
-	filterKernel	= new DSPCOMPLEX [filterSize];
-	Buffer		= new DSPCOMPLEX [filterSize];
-	ip		= 0;
-
-	for (i = 0; i < filterSize; i ++) {
-	   filterKernel [i]	= 0;
-	   Buffer [i]		= 0;
-	}
-}
-
-			~Basic_FIR (void) {
-	delete[]	filterKernel;
-	delete[]	Buffer;
-}
-//
-//	we process the samples backwards rather than reversing
-//	the kernel
-	DSPCOMPLEX		Pass (DSPCOMPLEX z) {
-int16_t	i;
-DSPCOMPLEX	tmp	= 0;
-
-	Buffer [ip]	= z;
-	for (i = 0; i < filterSize; i ++) {
-	   int16_t index = ip - i;
-	   if (index < 0)
-	      index += filterSize;
-	   tmp		+= Buffer [index] * filterKernel [i];
-	}
-
-	ip = (ip + 1) % filterSize;
-	return tmp;
-}
-
-	DSPFLOAT	Pass (DSPFLOAT v) {
-int16_t		i;
-DSPFLOAT	tmp	= 0;
-
-	Buffer [ip] = DSPCOMPLEX (v, 0);
-	for (i = 0; i < filterSize; i ++) {
-	   int16_t index = ip - i;
-	   if (index < 0)
-	      index += filterSize;
-	   tmp += real (Buffer [index]) * real (filterKernel [i]);
-	}
-
-	ip = (ip + 1) % filterSize;
-	return tmp;
-}
-
-};
-
-class	LowPassFIR : public Basic_FIR {
+class	Basic_FIR
+{
 public:
-			LowPassFIR (int16_t,	// order
-	                            int32_t, 	// cutoff frequency
-	                            int32_t	// samplerate
-	                           );
-			~LowPassFIR (void);
-	DSPCOMPLEX	*getKernel	(void);
-	void		newKernel	(int32_t);	// cutoff
+  int16_t    filterSize;
+  DSPCOMPLEX *filterKernel;
+  DSPCOMPLEX *Buffer;
+  int16_t    ip;
+  int32_t    sampleRate;
+
+  Basic_FIR(int16_t size)
+  {
+    int16_t i;
+
+    filterSize   = size;
+    filterKernel = new DSPCOMPLEX [filterSize];
+    Buffer       = new DSPCOMPLEX [filterSize];
+    ip           = 0;
+
+    for (i = 0; i < filterSize; i++)
+    {
+      filterKernel [i] = 0;
+      Buffer [i]       = 0;
+    }
+  }
+
+  ~Basic_FIR()
+  {
+    delete[]  filterKernel;
+    delete[]  Buffer;
+  }
+
+  //	we process the samples backwards rather than reversing
+  //	the kernel
+  DSPCOMPLEX Pass(DSPCOMPLEX z)
+  {
+    int16_t    i;
+    DSPCOMPLEX tmp = 0;
+    Buffer [ip] = z;
+
+    for (i = 0; i < filterSize; i++)
+    {
+      int16_t index = ip - i;
+
+      if (index < 0)
+      {
+        index += filterSize;
+      }
+
+      tmp += Buffer [index] * filterKernel [i];
+    }
+
+    ip = (ip + 1) % filterSize;
+    return tmp;
+  }
+
+  DSPFLOAT Pass(DSPFLOAT v)
+  {
+    int16_t  i;
+    DSPFLOAT tmp = 0;
+
+    Buffer [ip] = DSPCOMPLEX(v, 0);
+
+    for (i = 0; i < filterSize; i++)
+    {
+      int16_t index = ip - i;
+
+      if (index < 0)
+      {
+        index += filterSize;
+      }
+
+      tmp += real(Buffer [index]) * real(filterKernel [i]);
+    }
+
+    ip = (ip + 1) % filterSize;
+    return tmp;
+  }
+
 };
-//
+
+class	LowPassFIR : public Basic_FIR
+{
+public:
+  LowPassFIR(int16_t,  // order
+             int32_t,  // cutoff frequency
+             int32_t   // samplerate
+             );
+  ~LowPassFIR ();
+  DSPCOMPLEX  *getKernel(void);
+  void newKernel(int32_t);        // cutoff
+};
+
 //	Both for lowpass band bandpass, we provide:
-class	DecimatingFIR: public Basic_FIR {
+class	DecimatingFIR: public Basic_FIR
+{
 public:
-		         DecimatingFIR	(int16_t, int32_t, int32_t, int16_t);
-	                 DecimatingFIR	(int16_t, int32_t, int32_t,
-	                                                   int32_t, int16_t);
-			~DecimatingFIR	(void);
-	void		newKernel	(int32_t);
-	void		newKernel	(int32_t, int32_t);
-	bool		Pass	(DSPCOMPLEX, DSPCOMPLEX *);
-	bool		Pass	(DSPFLOAT, DSPFLOAT *);
-	DSPCOMPLEX	*getKernel	(void);
+  DecimatingFIR(int16_t, int32_t, int32_t, int16_t);
+  DecimatingFIR(int16_t, int32_t, int32_t, int32_t, int16_t);
+  ~DecimatingFIR  (void);
+
+  void newKernel(int32_t);
+  void newKernel(int32_t, int32_t);
+  bool Pass(DSPCOMPLEX, DSPCOMPLEX *);
+  bool Pass(DSPFLOAT, DSPFLOAT *);
+  DSPCOMPLEX *getKernel(void);
+
 private:
-	int16_t	decimationFactor;
-	int16_t	decimationCounter;
+  int16_t decimationFactor;
+  int16_t decimationCounter;
 };
 
-class	HighPassFIR: public Basic_FIR {
+class	HighPassFIR: public Basic_FIR
+{
 public:
-			HighPassFIR	(int16_t, int32_t, int32_t);
-			~HighPassFIR	(void);
-	void		newKernel	(int32_t);
+  HighPassFIR(int16_t, int32_t, int32_t);
+  ~HighPassFIR  (void);
+
+  void newKernel(int32_t);
 };
 
-class	BandPassFIR: public Basic_FIR {
+class	BandPassFIR: public Basic_FIR
+{
 public:
-			BandPassFIR	(int16_t, int32_t, int32_t, int32_t);
-			~BandPassFIR	(void);
-	DSPCOMPLEX	*getKernel	(void);
-	void		newKernel	(int32_t, int32_t);
+  BandPassFIR(int16_t, int32_t, int32_t, int32_t);
+  ~BandPassFIR  (void);
+
+  DSPCOMPLEX *getKernel(void);
+  void newKernel(int32_t, int32_t);
 private:
 };
 
-class	BasicBandPass: public Basic_FIR {
+class	BasicBandPass: public Basic_FIR
+{
 public:
-			BasicBandPass	(int16_t, int32_t, int32_t, int32_t);
-			~BasicBandPass	(void);
-	DSPCOMPLEX	*getKernel	(void);
+  BasicBandPass(int16_t, int32_t, int32_t, int32_t);
+  ~BasicBandPass  (void);
+
+  DSPCOMPLEX *getKernel(void);
 private:
 };
 
 class	adaptiveFilter {
 public:
-		adaptiveFilter	(int);
-		~adaptiveFilter	();
-	void	adaptFilter	(DSPFLOAT);
-	DSPCOMPLEX	Pass		(DSPCOMPLEX);
+  adaptiveFilter(int);
+  ~adaptiveFilter ();
+
+  void adaptFilter(DSPFLOAT);
+  DSPCOMPLEX Pass(DSPCOMPLEX);
 
 private:
-	int16_t		ip;
-	DSPFLOAT	err;
-	DSPFLOAT	mu;
-	int16_t		firsize;
-	DSPFLOAT	*Kernel;
-	DSPCOMPLEX	*Buffer;
+  int16_t    ip;
+  DSPFLOAT   err;
+  DSPFLOAT   mu;
+  int16_t    firsize;
+  DSPFLOAT   *Kernel;
+  DSPCOMPLEX *Buffer;
 };
 
-class HilbertFilter {
+class HilbertFilter
+{
 public:
-		HilbertFilter	(int16_t, DSPFLOAT, int32_t);
-		~HilbertFilter	();
-	DSPCOMPLEX	Pass		(DSPCOMPLEX);
-	DSPCOMPLEX	Pass		(DSPFLOAT, DSPFLOAT);
+  HilbertFilter(int16_t, DSPFLOAT, int32_t);
+  ~HilbertFilter();
+  DSPCOMPLEX Pass(DSPCOMPLEX);
+  DSPCOMPLEX Pass(DSPFLOAT, DSPFLOAT);
+
 private:
-	int16_t		ip;
-	int16_t		firsize;
-	int32_t		rate;
-	DSPFLOAT	*cosKernel;
-	DSPFLOAT	*sinKernel;
-	DSPCOMPLEX	*Buffer;
-	void		adjustFilter	(DSPFLOAT);
+  int16_t    ip;
+  int16_t    firsize;
+  int32_t    rate;
+  DSPFLOAT   *cosKernel;
+  DSPFLOAT   *sinKernel;
+  DSPCOMPLEX *Buffer;
+
+  void adjustFilter(DSPFLOAT);
 };
 
 #endif
-

704e68c2f46f893a4b842b5784358baca86d36ca
Thomas Neder <thomas.neder@freenet.de>
2022-04-30 22:51:24 +0200

    Beautify scope

--- includes/scopes-qwt6/scope.h
+++ includes/scopes-qwt6/scope.h
@@ -1,4 +1,3 @@
-#
 /*
  *    Copyright (C) 2008, 2009, 2010
  *    Jan van Katwijk (J.vanKatwijk@gmail.com)
@@ -6,7 +5,7 @@
  *
  *    This file is part of the SDR-J.
  *    Many of the ideas as implemented in SDR-J are derived from
- *    other work, made available through the GNU general Public License. 
+ *    other work, made available through the GNU general Public License.
  *    All copyrights of the original authors are recognized.
  *
  *    SDR-J is free software; you can redistribute it and/or modify
@@ -24,134 +23,133 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef	__SCOPES
-#define	__SCOPES
+#ifndef __SCOPES
+#define __SCOPES
 
-#include	"fm-constants.h"
-#include	<QObject>
-#include	<QStackedWidget>
-#include	<qwt.h>
-#include	<qwt_plot.h>
-#include	<qwt_plot_curve.h>
-#include	<qwt_plot_marker.h>
-#include	<qwt_plot_grid.h>
-#include	<qwt_color_map.h>
-#include	<qwt_plot_spectrogram.h>
-#include	<qwt_plot_zoomer.h>
-#include	<qwt_plot_textlabel.h>
-#include	<qwt_plot_panner.h>
-#include	<qwt_plot_layout.h>
-#include	<qwt_scale_widget.h>
-#include	<QBrush>
-#include	<QPen>
-#include	<QTimer>
-#include	<stdint.h>
-#include	"spectrogramdata.h"
+#include "fm-constants.h"
+#include "spectrogramdata.h"
+#include <QBrush>
+#include <QObject>
+#include <QPen>
+#include <QStackedWidget>
+#include <QTimer>
+#include <qwt.h>
+#include <qwt_color_map.h>
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_plot_layout.h>
+#include <qwt_plot_marker.h>
+#include <qwt_plot_panner.h>
+#include <qwt_plot_spectrogram.h>
+#include <qwt_plot_textlabel.h>
+#include <qwt_plot_zoomer.h>
+#include <qwt_scale_widget.h>
+#include <stdint.h>
 
-class	SpectrumViewer;
-class	WaterfallViewer;
-class	QwtLinearColorMap;
-class	QwtScaleWidget;
+class SpectrumViewer;
+class WaterfallViewer;
+class QwtLinearColorMap;
+class QwtScaleWidget;
 /*
  *	The stacked object is called the scope, it is built as a wrapper
  *	around the actual display mechanisms the waterfall and the
  *	spectrumviewer.
  */
 
-#define	WATERFALL_MODE	0
-#define	SPECTRUM_MODE	1
+#define WATERFALL_MODE 0
+#define SPECTRUM_MODE 1
 
-class Scope: public QObject {
-Q_OBJECT
+class Scope : public QObject {
+  Q_OBJECT
 public:
-	Scope (QwtPlot *, uint16_t, uint16_t);
-	~Scope (void);
-void	Display 	(double *, double *, double, int32_t);
-void	SelectView	(uint8_t);
-void	setBitDepth	(int16_t);
-int	currentMode	(void);
+  Scope(QwtPlot *, uint16_t, uint16_t);
+  ~Scope(void);
+  void Display(double *, double *, double, int32_t);
+  void SelectView(uint8_t);
+  void setBitDepth(int16_t);
+  int currentMode(void);
+
 private:
-	QwtPlot		*Plotter;
-	uint16_t	Displaysize;
-	uint16_t	Rastersize;
-	uint8_t		CurrentWidget;
-	SpectrumViewer	*Spectrum;
-	WaterfallViewer	*Waterfall;
-	int16_t		bitDepth;
-	float		get_db		(float);
+  QwtPlot *Plotter;
+  uint16_t Displaysize;
+  uint16_t Rastersize;
+  uint8_t CurrentWidget;
+  SpectrumViewer *Spectrum;
+  WaterfallViewer *Waterfall;
+  int16_t bitDepth;
+  float get_db(float);
 private slots:
-	void		leftClicked	(int);
-	void		rightClicked	(int);
+  void leftClicked(int);
+  void rightClicked(int);
 signals:
-	void		clickedwithLeft	(int);
-	void		clickedwithRight	(int);
+  void clickedwithLeft(int);
+  void clickedwithRight(int);
 };
 
 /*
  *	for the waterfall display
  */
-class WaterfallViewer: public QObject, public QwtPlotSpectrogram {
-Q_OBJECT
+class WaterfallViewer : public QObject, public QwtPlotSpectrogram {
+  Q_OBJECT
 public:
-	WaterfallViewer		(QwtPlot *, uint16_t, uint16_t);
-	~WaterfallViewer	();
-void	ViewWaterfall 		(double *,
-	                         double *,
-	                         double,
-	                         int32_t);
+  WaterfallViewer(QwtPlot *, uint16_t, uint16_t);
+  ~WaterfallViewer();
+  void ViewWaterfall(double *, double *, double, int32_t);
+
 private:
-	SpectrogramData	*WaterfallData;
-	QwtPlot		*plotgrid;
-	uint16_t	Displaysize;
-	uint16_t	Rastersize;
-	double		*plotData;
-	QwtPlotMarker	*Marker;
-	uint32_t	IndexforMarker;
-	QwtPlotPicker	*lm_picker;
-	QwtPlotPicker	*rm_picker;
-	uint8_t		OneofTwo;
-	QwtLinearColorMap	*colorMap;
-	QwtScaleWidget		*rightAxis;
+  SpectrogramData *WaterfallData;
+  QwtPlot *plotgrid;
+  uint16_t Displaysize;
+  uint16_t Rastersize;
+  double *plotData;
+  QwtPlotMarker *Marker;
+  uint32_t IndexforMarker;
+  QwtPlotPicker *lm_picker;
+  QwtPlotPicker *rm_picker;
+  uint8_t OneofTwo;
+  QwtLinearColorMap *colorMap;
+  QwtScaleWidget *rightAxis;
 private slots:
-	void	leftMouseClick (const QPointF &);
-	void	rightMouseClick (const QPointF &);
+  void leftMouseClick(const QPointF &);
+  void rightMouseClick(const QPointF &);
 signals:
-	void	leftClicked (int);
-	void	rightClicked (int);
+  void leftClicked(int);
+  void rightClicked(int);
 };
 /*
  *	The spectrumDisplay
  */
-class	SpectrumViewer: public QObject {
-Q_OBJECT
+class SpectrumViewer : public QObject {
+  Q_OBJECT
 public:
-	SpectrumViewer 	(QwtPlot *, uint16_t);
-	~SpectrumViewer	(void);
-void	ViewSpectrum	(double *, double *, double, int32_t);
-void	setBitDepth	(int16_t);
+  SpectrumViewer(QwtPlot *, uint16_t);
+  ~SpectrumViewer(void);
+  void ViewSpectrum(double *, double *, double, int32_t);
+  void setBitDepth(int16_t);
+
 private:
-	QwtPlot		*plotgrid;
-	uint16_t	Displaysize;
-	QwtPlotGrid	*grid;
-	QwtPlotCurve	*SpectrumCurve;
-	QwtPlotMarker	*Marker;
-	uint32_t	IndexforMarker;
-	QwtPlotPicker	*lm_picker;
-	QwtPlotPicker	*rm_picker;
-	QBrush		*ourBrush;
-	int16_t		bitDepth;
-	int32_t		normalizer;
-	float		get_db		(float);
-	int		oldmarkerValue;
-	int		counter;
-	QwtPlotTextLabel *maxLabel;
-	QwtPlotTextLabel *minLabel;
+  QwtPlot *plotgrid;
+  uint16_t Displaysize;
+  QwtPlotGrid *grid;
+  QwtPlotCurve *SpectrumCurve;
+  QwtPlotMarker *Marker;
+  uint32_t IndexforMarker;
+  QwtPlotPicker *lm_picker;
+  QwtPlotPicker *rm_picker;
+  QBrush *ourBrush;
+  int16_t bitDepth;
+  int32_t normalizer;
+  float get_db(float);
+  int oldmarkerValue;
+  int counter;
+  QwtPlotTextLabel *maxLabel;
+  QwtPlotTextLabel *minLabel;
 private slots:
-	void	leftMouseClick (const QPointF &);
-	void	rightMouseClick (const QPointF &);
+  void leftMouseClick(const QPointF &);
+  void rightMouseClick(const QPointF &);
 signals:
-	void	leftClicked	(int);
-	void	rightClicked	(int);
+  void leftClicked(int);
+  void rightClicked(int);
 };
 #endif
-
